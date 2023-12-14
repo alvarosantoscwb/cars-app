dbf57ffdfa848d9239906a2c434eac079315e206
sleeptd2 <alvarojuniorctb@hotmail.com>
2023-12-14 15:10:44 -0300

    Alteração do titulo, descrição,button text e imagem.

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,22 +7,21 @@ const Hero = () => {
   return (
     <div className="hero">
       <div className="flex-1 pt-36 padding-x">
-        <h1 className="hero__title">
-          Find, book, or rent a car -- quickly and easily!
-        </h1>
+        <h1 className="hero__title">Encontre o carro dos seus sonhos</h1>
         <p className="hero__subtitle">
-          Streamline your car rental experience with our effortless booking
-          process.
+          O veículo dos seus sonhos está aqui embarque em uma jornada de
+          excelência automotiva conosco.
         </p>
         <CustomButton
-          title="Explore Cars"
+          title="Encontre aqui"
           containerStyles="bg-primary-blue text-white rounded-full mt-10"
           handleClick={handleScroll}
         ></CustomButton>
       </div>
       <div className="hero__image-container">
         <div className="hero__image">
-          <Image src="./hero.png" alt="hero" />
+          <Image src="/lambo.png" alt="hero" fill className="object-contain" />
+          <div className="hero__image-overlay"></div>
         </div>
       </div>
     </div>
