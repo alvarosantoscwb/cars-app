4c7c7b1a3f098799dd76560131d28af50f25ea27
sleeptd2 <alvarojuniorctb@hotmail.com>
2023-12-14 17:47:08 -0300

    Adicionado components novos

---
+++ constants/index.ts
@@ -0,0 +1,99 @@
+export const manufacturers = [
+  "Acura",
+  "Alfa Romeo",
+  "Aston Martin",
+  "Audi",
+  "Bentley",
+  "BMW",
+  "Buick",
+  "Cadillac",
+  "Chevrolet",
+  "Chrysler",
+  "Citroen",
+  "Dodge",
+  "Ferrari",
+  "Fiat",
+  "Ford",
+  "GMC",
+  "Honda",
+  "Hyundai",
+  "Infiniti",
+  "Jaguar",
+  "Jeep",
+  "Kia",
+  "Lamborghini",
+  "Land Rover",
+  "Lexus",
+  "Lincoln",
+  "Maserati",
+  "Mazda",
+  "McLaren",
+  "Mercedes-Benz",
+  "MINI",
+  "Mitsubishi",
+  "Nissan",
+  "Porsche",
+  "Ram",
+  "Rolls-Royce",
+  "Subaru",
+  "Tesla",
+  "Toyota",
+  "Volkswagen",
+  "Volvo",
+];
+
+export const yearsOfProduction = [
+  { title: "Year", value: "" },
+  { title: "2015", value: "2015" },
+  { title: "2016", value: "2016" },
+  { title: "2017", value: "2017" },
+  { title: "2018", value: "2018" },
+  { title: "2019", value: "2019" },
+  { title: "2020", value: "2020" },
+  { title: "2021", value: "2021" },
+  { title: "2022", value: "2022" },
+  { title: "2023", value: "2023" },
+];
+
+export const fuels = [
+  {
+    title: "Combustivel",
+    value: "",
+  },
+  {
+    title: "Gasolina",
+    value: "Gas",
+  },
+  {
+    title: "Eletrico",
+    value: "Electricity",
+  },
+];
+
+export const footerLinks = [
+  {
+    title: "Sobre nós",
+    links: [
+      { title: "How it works", url: "/" },
+      { title: "Featured", url: "/" },
+      { title: "Partnership", url: "/" },
+      { title: "Bussiness Relation", url: "/" },
+    ],
+  },
+  {
+    title: "Empresa",
+    links: [
+      { title: "Events", url: "/" },
+      { title: "Blog", url: "/" },
+      { title: "Podcast", url: "/" },
+      { title: "Invite a friend", url: "/" },
+    ],
+  },
+  {
+    title: "Redes Sociais",
+    links: [
+      { title: "Instagram", url: "/" },
+      { title: "Facebook", url: "/" },
+    ],
+  },
+];
